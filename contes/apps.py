805ad705c110faec9750e3510517970b4b00c38b
tytyne <dusaflora2@gmail.com>
2019-03-25 17:48:01 +0200

    first commit

---
+++ contes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ContesConfig(AppConfig):
+    name = 'contes'
---
+++ hells/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HellsConfig(AppConfig):
+    name = 'hells'
